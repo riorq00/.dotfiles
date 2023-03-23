84db65bad832924d11b7b2cc0bab39f78b777320
riorq00 <reworkk7@gmail.com>
2023-03-23 01:07:38 -0300

    feat(awesome): update awesome file

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -81,22 +81,10 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
 	awful.layout.suit.tile,
-	--awful.layout.suit.tile.left,
-	--[[
-  awful.layout.suit.tile.bottom,
+	awful.layout.suit.tile.left,
+	awful.layout.suit.tile.bottom,
 	awful.layout.suit.tile.top,
 	awful.layout.suit.fair,
-	awful.layout.suit.fair.horizontal,
-	awful.layout.suit.spiral,
-	awful.layout.suit.spiral.dwindle,
-	awful.layout.suit.max,
-	awful.layout.suit.max.fullscreen,
-	awful.layout.suit.magnifier,
-	awful.layout.suit.corner.nw,
-	awful.layout.suit.corner.ne,
-	awful.layout.suit.corner.sw,
-	awful.layout.suit.corner.se,
-  ]]
 }
 -- }}}
 
@@ -262,7 +250,7 @@ awful.screen.connect_for_each_screen(function(s)
 			mytextclock,
 			tbox_separator,
 			brightness_widget({
-				--type = "icon_and_text",
+				type = "icon_and_text",
 				program = "brightnessctl",
 				step = 2,
 			}),
@@ -270,7 +258,7 @@ awful.screen.connect_for_each_screen(function(s)
 			volume_widget({
 				mixer_cmd = "pavucontrol",
 				--widget_type = "icon_and_text",
-				widget_type = "arc",
+				widget_type = "icon",
 				mute_color = "#CB4B16",
 				with_icon = true,
 				step = 2,
@@ -288,8 +276,9 @@ awful.screen.connect_for_each_screen(function(s)
 			tbox_separator,
 			wibox.widget.systray(),
 			tbox_separator,
-			--s.mylayoutbox,
 			logout_menu_widget(),
+			tbox_separator,
+			s.mylayoutbox,
 		},
 	})
 end)
