0621022c083215d031038341701216a97788b17f
riorq00 <reworkk7@gmail.com>
2023-03-26 22:41:23 -0300

    refactor(awesome): renames the widgets folder

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,33 +1,18 @@
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
-local brightness_widget = require("awesome-wm-widgets.brightness-widget.brightness")
-
--- Standard awesome library
+local brightness_widget = require("widgets.brightness-widget.brightness")
 local gears = require("gears")
 local awful = require("awful")
 require("awful.autofocus")
--- Widget and layout library
---local awidgets = require("awesome-wm-widgets")
---local lain = require("lain")
-local logout_menu_widget = require("awesome-wm-widgets.logout-menu-widget.logout-menu")
+local logout_menu_widget = require("widgets.logout-menu-widget.logout-menu")
 local wibox = require("wibox")
-local volume_widget = require("awesome-wm-widgets.volume-widget.volume")
-local wifi_widget = require("awesome-wm-widgets.wifi-widget.wifi")
-local battery_widget = require("awesome-wm-widgets.battery-widget.battery")
--- Theme handling library
+local volume_widget = require("widgets.volume-widget.volume")
+local battery_widget = require("widgets.battery-widget.battery")
 local beautiful = require("beautiful")
--- Notification library
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
 	naughty.notify({
 		preset = naughty.config.presets.critical,
@@ -36,7 +21,6 @@ if awesome.startup_errors then
 	})
 end
 
--- Handle runtime errors after startup
 do
 	local in_error = false
 	awesome.connect_signal("debug::error", function(err)
@@ -56,32 +40,18 @@ do
 end
 -- }}}
 
--- widgets
-
--- Separator
--- tbox_separator = wibox.widget.textbox("  ")
 tbox_separator = wibox.widget.textbox("  ")
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
---beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 beautiful.init(gears.filesystem.get_themes_dir() .. "riorq/theme.lua")
 
--- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 browser = "brave"
 fm = "thunar"
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
--- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
 	awful.layout.suit.tile,
 	awful.layout.suit.tile.left,
@@ -89,10 +59,7 @@ awful.layout.layouts = {
 	awful.layout.suit.tile.top,
 	awful.layout.suit.fair,
 }
--- }}}
 
--- {{{ Menu
--- Create a launcher widget and a main menu
 myawesomemenu = {
 	{
 		"hotkeys",
@@ -515,7 +482,7 @@ awful.rules.rules = {
 	{
 		rule = {},
 		properties = {
-			border_width = 1,
+			border_width = 2,
 			border_color = beautiful.border_normal,
 			focus = awful.client.focus.filter,
 			raise = true,
@@ -641,7 +608,8 @@ client.connect_signal("unfocus", function(c)
 end)
 -- }}}
 
-beautiful.useless_gap = 4
+-- GAPS
+beautiful.useless_gap = 5
 
 -- Autostart
 awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &")
