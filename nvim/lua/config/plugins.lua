e0752fda541553fd6762f9645d3be767ef8452e6
riorq00 <reworkk7@gmail.com>
2023-04-05 20:45:49 -0300

    feat(nvim): add peek

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -202,6 +202,15 @@ lazy.setup({
   },
 
   {
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    config = function()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
+
+  {
     "dinhhuy258/git.nvim",
   }, -- For git blame & browse
 }, {
