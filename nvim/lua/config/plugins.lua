83e590f3fd3e26697b1d1498867b616b0abedc43
riorq00 <reworkk7@gmail.com>
2023-04-12 16:21:54 -0300

    chore(nvim): adds cmd option to peek

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -223,6 +223,7 @@ lazy.setup({
   {
     "toppair/peek.nvim",
     build = "deno task --quiet build:fast",
+    cmd = { "PeekOpen", "PeekClose" },
     config = function()
       vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
       vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
