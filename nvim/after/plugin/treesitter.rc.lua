d7b0e0d81ff43c993754f22ab321f3e38f40e7ec
riorq00 <reworkk7@gmail.com>
2023-04-25 16:29:05 -0300

    chore(nvim): uncoments parser_config

--- nvim/after/plugin/treesitter.rc.lua
+++ nvim/after/plugin/treesitter.rc.lua
@@ -22,8 +22,7 @@ ts.setup({
   autotag = {
     enable = true,
   },
-  additional_vim_regex_highlighting = false,
 })
 
---[[ local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" } ]]
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
