ec126756ac2b008d516a875b77f8c40de6b4e076
riorq00 <reworkk7@gmail.com>
2023-04-25 16:39:09 -0300

    feat(zsh): adds a script to disable mouse acceleration

---
+++ zsh/mouse.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xinput --set-prop 'pointer:''YSPRINGTECH USB OPTICAL MOUSE' 'libinput Accel Profile Enabled' 0, 1
+#xinput --set-prop 'pointer:''Your mouse name here' 'libinput Accel Speed' -0.1
+
