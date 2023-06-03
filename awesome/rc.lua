d3ffaaa0604ec011f3eec05b010f12a5e35a6d5e
riorq00 <reworkk7@gmail.com>
2023-06-03 16:18:33 -0300

    chore(awesome): change tags

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -162,7 +162,8 @@ awful.screen.connect_for_each_screen(function(s)
 
 	-- Each screen has its own tag table.
 	--awful.tag({ "", "", "", "", "", "", "", "", "" }, s, awful.layout.layouts[1])
-	awful.tag({ "q", "i", "y", "a", "n", "a" }, s, awful.layout.layouts[1])
+	awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+	--awful.tag({ "q", "i", "y", "a", "n", "a" }, s, awful.layout.layouts[1])
 
 	-- Create a promptbox for each screen
 	s.mypromptbox = awful.widget.prompt()
