298649e981c90c8c4b50d28026afff408ba20081
riorq00 <reworkk7@gmail.com>
2023-06-03 16:22:23 -0300

    feat(nvim): adds new maps

--- nvim/lua/config/maps.lua
+++ nvim/lua/config/maps.lua
@@ -3,6 +3,16 @@ local opts = { noremap = true, silent = true }
 
 keymap.set("n", "x", '"_x')
 
+-- navigate within insert mode
+keymap.set("i", "<C-h>", "<Left>", opts)
+keymap.set("i", "<C-l>", "<Right>", opts)
+keymap.set("i", "<C-j>", "<Down>", opts)
+keymap.set("i", "<C-k>", "<Up>", opts)
+
+-- line numbers
+keymap.set("n", "<leader>n", "<cmd> set nu! <CR>")
+keymap.set("n", "<leader>rn", "<cmd> set rnu! <CR>")
+
 -- Format Prettier
 keymap.set("n", "pr", ":Prettier<CR>")
 
@@ -52,9 +62,50 @@ keymap.set({ "n", "v" }, "tran", require("config.translates.init"), opts)
 -- Autotranslate to english
 keymap.set("n", "tra", "<S-v>:Translate en<CR>", opts)
 
-vim.cmd([[
-  map <C-K> :pyf /usr/share/clang/clang-format.py<cr>
-  imap <C-K> <c-o>:pyf /usr/share/clang/clang-format.py<cr>
-]])
+-- dap
+local function sidebarOpen()
+  local widgets = require("dap.ui.widgets")
+  local sidebar = widgets.sidebar(widgets.scopes)
+  sidebar.open()
+end
+keymap.set("n", "<Leader>db", "<cmd> DapToggleBreakpoint <CR>", opts)
+keymap.set("n", "<Leader>dus", sidebarOpen, opts)
 
--- vim.g.phyton3_host_prog = "/usr/local/bin/python3"
+keymap.set("n", "<F5>", function()
+  require("dap").continue()
+end)
+keymap.set("n", "<F10>", function()
+  require("dap").step_over()
+end)
+keymap.set("n", "<F11>", function()
+  require("dap").step_into()
+end)
+keymap.set("n", "<F12>", function()
+  require("dap").step_out()
+end)
+keymap.set("n", "<Leader>B", function()
+  require("dap").set_breakpoint()
+end)
+keymap.set("n", "<Leader>lp", function()
+  require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+end)
+keymap.set("n", "<Leader>dr", function()
+  require("dap").repl.open()
+end)
+keymap.set("n", "<Leader>dl", function()
+  require("dap").run_last()
+end)
+keymap.set({ "n", "v" }, "<Leader>dh", function()
+  require("dap.ui.widgets").hover()
+end)
+keymap.set({ "n", "v" }, "<Leader>dp", function()
+  require("dap.ui.widgets").preview()
+end)
+keymap.set("n", "<Leader>df", function()
+  local widgets = require("dap.ui.widgets")
+  widgets.centered_float(widgets.frames)
+end)
+keymap.set("n", "<Leader>ds", function()
+  local widgets = require("dap.ui.widgets")
+  widgets.centered_float(widgets.scopes)
+end)
