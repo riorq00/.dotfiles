2b9540bcd7294b3729b99723aa04dfef50565896
riorq00 <reworkk7@gmail.com>
2023-08-28 18:48:53 -0300

    chore(nvim): update lspsaga

--- nvim/plugin/lspsaga.rc.lua
+++ nvim/plugin/lspsaga.rc.lua
@@ -9,9 +9,6 @@ saga.setup({
   ui = {
     border = "rounded",
     -- theme = "round",
-    colors = {
-      normal_bg = "#002b36",
-    },
   },
   lightbulb = {
     enable = true,
@@ -19,6 +16,9 @@ saga.setup({
     sign = true,
     sign_priority = 40,
     virtual_text = true,
+  },
+  outline = {
+    layout = "float",
   },
   hover = {
     open_browser = "!brave",
@@ -31,7 +31,7 @@ map.set("n", "<C-j>", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
 map.set("n", "C-o", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 map.set("n", "gl", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
 map.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
-map.set("n", "gf", "<Cmd>Lspsaga lsp_finder<CR>", opts)
+map.set("n", "gf", "<Cmd>Lspsaga finder<CR>", opts)
 map.set("i", "<C-v>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 
 map.set("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
