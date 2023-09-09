cfc16eed1950537b687829d89f99afbe576bbb5e
riorq00 <reworkk7@gmail.com>
2023-09-09 01:59:04 -0300

    chore(awesome): remove mouse interaction

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -275,9 +275,9 @@ end)
 root.buttons(mytable.join(
 	awful.button({}, 3, function()
 		awful.util.mymainmenu:toggle()
-	end),
-	awful.button({}, 4, awful.tag.viewnext),
-	awful.button({}, 5, awful.tag.viewprev)
+	end)
+	-- awful.button({}, 4, awful.tag.viewnext),
+	-- awful.button({}, 5, awful.tag.viewprev)
 ))
 
 -- }}}
